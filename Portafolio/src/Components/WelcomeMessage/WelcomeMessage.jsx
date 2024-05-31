299141cade7eca33ad3d35a2f8246432a6cc47c5
ppguz <ppguzmanmayen@gmail.com>
2024-05-30 22:20:49 -0600

    Feat: education styles

--- Portafolio/src/Components/WelcomeMessage/WelcomeMessage.jsx
+++ Portafolio/src/Components/WelcomeMessage/WelcomeMessage.jsx
@@ -16,7 +16,7 @@ const WelcomeMessage = () => {
               `}
             </pre>
           </h1>
-          <p>Welcome I'm PPGM (Pedro Pablo Guzman Mayen) and this is my portafolio, you can use the help command to learn about me, see my job and there's a secret command to get a surpirse, try to find it 😉</p>
+          <p className = 'message'>Welcome I'm PPGM (Pedro Pablo Guzman Mayen) and this is my poRtafolio, you can use the help command to learn about me, see my job and there's a secret command to get a surpirse, try to find it 😉</p>
         </div>
       </li>
   );
